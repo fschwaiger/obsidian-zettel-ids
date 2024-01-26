3055dc53f12c8ee3230682a92e8362165cba73f2
Florian Schwaiger <f.schwaiger@tum.de>
2024-01-26 16:31:34 +0100

    feat!: inital draft of plugin

--- main.ts
+++ main.ts
@@ -1,134 +1,199 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	Editor,
+	MarkdownView,
+	Notice,
+	Plugin,
+} from "obsidian";
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
 	async onload() {
-		await this.loadSettings();
+		let generateCallback = (zettelIdMethod: Function) => {
+			return (editor: Editor, view: MarkdownView) => {
+				let folder = view.file?.parent?.path ?? "";
+				let filename = view.file?.name;
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				if (!filename) {
+					new Notice("Please save the file first.");
+					return;
 				}
-			}
+
+				let zettelId = this.getZettelIdFromFilename(filename);
+
+				if (!zettelId) {
+					new Notice("Please name the file with a zettel ID prefix.");
+					return;
+				}
+
+				let nextZettelId = zettelIdMethod(zettelId);
+				let nextZettelFilename = nextZettelId + ".md";
+				let data = editor.getSelection();
+
+				// create a new file with a name prefixed with this id
+				this.app.vault
+					.create(folder + "/" + nextZettelFilename, "")
+					.then((file) => {
+						// add a link to the new file
+						editor.replaceSelection("[[ " + nextZettelId + " ]]");
+						// open the new file
+						this.app.workspace.openLinkText(
+							nextZettelFilename,
+							data,
+							true
+						);
+					})
+					.catch((error) => {
+						new Notice("Error creating file: " + error);
+					});
+			};
+		};
+
+		// add a command to create a child zettel note
+		this.addCommand({
+			id: "create-child-zettel",
+			name: "Create Child Zettel",
+			editorCallback: generateCallback(
+				this.getNextChildZettelId.bind(this)
+			),
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		// add a command to create a sibling zettel note
+		this.addCommand({
+			id: "create-sibling-zettel",
+			name: "Create Sibling Zettel",
+			editorCallback: generateCallback(
+				this.getNextSiblingZettelId.bind(this)
+			),
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// add a command to create the next viable zettel note
+		this.addCommand({
+			id: "create-next-zettel",
+			name: "Create Next Zettel",
+			editorCallback: generateCallback(this.getNextZettelId.bind(this)),
+		});
+
+		// add a command to open the parent note
+		this.addCommand({
+			id: "open-parent-zettel",
+			name: "Open Parent Zettel",
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				if (!view.file?.name) {
+					new Notice("Please save the file first.");
+					return;
+				}
+
+				let zettelId = this.getZettelIdFromFilename(view.file?.name);
+
+				if (!zettelId) {
+					new Notice("Please name the file with a zettel ID prefix.");
+					return;
+				}
+
+				let parentZettelId = this.getParentZettelId(zettelId);
+
+				if (!parentZettelId) {
+					new Notice("Parent zettel not found.");
+					return;
+				}
+
+				let parentFile = this.app.vault
+					.getMarkdownFiles()
+					.find((file) => {
+						return file.name.startsWith(parentZettelId + " ");
+					});
+
+				if (!parentFile) {
+					new Notice("Parent file not found.");
+					return;
+				}
+
+				this.app.workspace.openLinkText(parentFile.name, "", true);
+			},
+		});
 	}
 
-	onunload() {
+	onunload() {}
 
+	getZettelIdFromFilename(filename: string): string {
+		if (filename.match(/^[0-9a-zA-Z]+.md$/)) {
+			return filename.split(".")[0];
+		}
+
+		return filename.split(/\W+/)[0];
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	getNextChildZettelId(parentZettelId: string): string {
+		// find all existing child files
+		let nextChildIndex = this.app.vault
+			.getMarkdownFiles()
+			.map((file) => {
+				return file.name.startsWith(parentZettelId) &&
+					!file.name.startsWith(parentZettelId + " ")
+					? this.getZettelIdFromFilename(file.name)
+					: undefined;
+			})
+			.filter((zettelId) => {
+				return zettelId !== undefined;
+			})
+			.map((zettelId: string) => {
+				// get the last character or number of the zettel id
+				let charsOrDigits = zettelId.replace(RegExp('^' + parentZettelId), '');
+				// if chars is a number, return the number, otherwise return the char index
+				if (charsOrDigits.match(/^[0-9]+$/)) {
+					return parseInt(charsOrDigits);
+				} else {
+					// each letter is a number, a == 1, aa == 27, A == 1, AA == 27
+					return charsOrDigits.toLowerCase().split("").reduce((acc, char) => {
+						return acc * 26 + char.charCodeAt(0) - 96;
+					}, 0);
+				}
+			}).reduce((acc, index) => {
+				return Math.max(acc, index);
+			}, 0) + 1;
+
+		return this.getChildZettelId(parentZettelId, nextChildIndex);
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	getChildZettelId(parentZettelId: string, childIndex: number): string {
+		if (parentZettelId.match(/[a-zA-Z]+$/)) {
+			// if the parent ID ends with a letter, append a number
+			return parentZettelId + childIndex;
+		} else if (parentZettelId == parentZettelId.toUpperCase()) {
+			// if the parent ID is all caps, append a number, A == 1
+			return parentZettelId + String.fromCharCode(64 + childIndex);
+		} else {
+			// append a letter, a == 1
+			return parentZettelId + String.fromCharCode(96 + childIndex);
+		}
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+	getNextSiblingZettelId(siblingZettelId: string): string {
+		return this.getNextChildZettelId(
+			this.getParentZettelId(siblingZettelId)
+		);
 	}
 
-	display(): void {
-		const {containerEl} = this;
+	getParentZettelId(childZettelId: string): string {
+		if (childZettelId.match(/[a-zA-Z]+$/)) {
+			// if the child zettel id ends with a letter, remove the letter
+			return childZettelId.replace(/[a-zA-Z]+$/, "");
+		} else {
+			// if the child zettel id ends with digits, remove the digits
+			return childZettelId.replace(/[0-9]+$/, "");
+		}
+	}
 
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	getNextZettelId(prevZettelId: string): string {
+		let nextZettelId = this.getNextSiblingZettelId(prevZettelId);
+		// if the next ID does not end in 1 or A, use a child id instead
+		if (
+			!nextZettelId.match(/[aA]$/) &&
+			!nextZettelId.match(/(?<=[a-zA-Z])1$/)
+		) {
+			nextZettelId = this.getNextChildZettelId(prevZettelId);
+		}
+		return nextZettelId;
 	}
 }
